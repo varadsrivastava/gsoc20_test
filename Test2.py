bdb4a3d37c0dbca2f24ae3cce1745deb5741fcaa
VARAD SRIVASTAVA <varad.srivastava26@gmail.com>
2020-03-29 17:04:34 +0530

    Mark II Final

---
+++ Test2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 16:06:58 2020
+
+@author: Varad Srivastava
+"""
+
+## A simple CNN on MNIST without using estimator API
+
+import tensorflow as tf
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
